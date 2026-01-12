04cd701f3b05dbf158f243e9abf2fb02a20da85c
harmanshergill <60117202+harmanshergill@users.noreply.github.com>
2026-01-12 23:21:14 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -8,15 +8,18 @@ function press(n){
   updateDots();
   if(pin.length===6)setTimeout(check,150);
 }
+
 function del(){
   pin=pin.slice(0,-1);
   updateDots();
 }
+
 function updateDots(){
   document.querySelectorAll('.dots span').forEach((d,i)=>{
     d.classList.toggle('filled',i<pin.length);
   });
 }
+
 function check(){
   if(pin===correct){
     location.href='loading.html';
