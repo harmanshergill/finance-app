da298b388df33d39aed940f10a0a42bf62bb29bd
harmanshergill <60117202+harmanshergill@users.noreply.github.com>
2026-01-12 23:07:10 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,32 +2,26 @@
 let pin = "";
 const correct = "260400";
 
-function press(n) {
-  if (pin.length >= 6) return;
-  pin += n;
+function press(n){
+  if(pin.length>=6) return;
+  pin+=n;
   updateDots();
-
-  if (pin.length === 6) {
-    setTimeout(check, 200);
-  }
+  if(pin.length===6) setTimeout(check,150);
 }
-
-function del() {
-  pin = pin.slice(0, -1);
+function del(){
+  pin=pin.slice(0,-1);
   updateDots();
 }
-
-function updateDots() {
-  document.querySelectorAll('.dots span').forEach((d, i) => {
-    d.classList.toggle('filled', i < pin.length);
+function updateDots(){
+  document.querySelectorAll('.dots span').forEach((d,i)=>{
+    d.classList.toggle('filled', i<pin.length);
   });
 }
-
-function check() {
-  if (pin === correct) {
-    window.location.href = "loading.html";
+function check(){
+  if(pin===correct){
+    window.location.href='loading.html';
   } else {
-    pin = "";
+    pin='';
     updateDots();
   }
 }
