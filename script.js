065a935f1d9444bf05f44f226e8c4f1e887193db
harmanshergill <60117202+harmanshergill@users.noreply.github.com>
2026-01-12 23:01:27 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+
+let pin = "";
+const correct = "260400";
+
+function press(n) {
+  if (pin.length >= 6) return;
+  pin += n;
+  updateDots();
+
+  if (pin.length === 6) {
+    setTimeout(check, 200);
+  }
+}
+
+function del() {
+  pin = pin.slice(0, -1);
+  updateDots();
+}
+
+function updateDots() {
+  document.querySelectorAll('.dots span').forEach((d, i) => {
+    d.classList.toggle('filled', i < pin.length);
+  });
+}
+
+function check() {
+  if (pin === correct) {
+    window.location.href = "loading.html";
+  } else {
+    pin = "";
+    updateDots();
+  }
+}
